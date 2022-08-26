77d2851e4258dcc060dcdd75710578ded26b111d
ebuenting5 <buenting5@tritons.iowacentral.edu>
2022-08-26 10:55:13 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,12 @@
+/*
+	Ethan Buenting
+	C++ Fall 2022
+	Due: 08/29/2022
+	Lab 1 Introduction to C++ Programming
+	This lab is a demo of my ability to work with an IDE and Github
+*/
+#include <iostream>
+int main() {
+	std::cout << "Hello World!" << " Orange";
+	return 0;
+}
